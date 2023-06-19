ff1f96cc3cbdc1d5741feb689952228ef51a2800
pefi <pefish1990@gmail.com>
2023-06-19 09:32:04 +0330

    Firsty

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <ul className="Nav">
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Products</a>
+        </li>
+        <li>
+          <a href="">Our Team</a>
+        </li>
+        <li className="sam">
+          <a>Contacts</a>
+        </li>
+      </ul>      
+      <div className="Cover">Cover</div>
+      <div className="grid-container">
+        <div className="grid-item">1</div>
+        <div className="grid-item">2</div>
+        <div className="grid-item">3</div>
+        <div className="grid-item">4</div>
+        <div className="grid-item">5</div>
+        <div className="grid-item">6</div>
+        <div className="grid-item">7</div>
+        <div className="grid-item">8</div>
+        <div className="grid-item">9</div>
+        <div className="grid-item">10</div>
+        <div className="grid-item">11</div>
+        <div className="grid-item">12</div>
+        <div className="grid-item">13</div>
+        <div className="grid-item">14</div>
+        <div className="grid-item">15</div>
+        <div className="grid-item">16</div>
+      </div>
+    </>
+  );
+};
+
+export default App;
